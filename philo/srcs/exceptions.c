43e19e7f09705c52590a8e744f6d77a653bb4c61
Gaylor LAURENT <galauren@student.42.fr>
2025-08-28 05:20:07 +0200

    autosave

--- philo/srcs/exceptions.c
+++ philo/srcs/exceptions.c
@@ -6,7 +6,7 @@
 /*   By: galauren <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 17:37:42 by galauren          #+#    #+#             */
-/*   Updated: 2025/08/25 22:58:11 by galauren         ###   ########.fr       */
+/*   Updated: 2025/08/28 05:17:39 by galauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,11 @@ void	philo_status_print(t_philo_list *p, char *msg)
 	pthread_mutex_lock(&(p->tblptr->death_lock));
 	pthread_mutex_lock(&(p->tblptr->enough_lock));
 	if (!p->tblptr->stop_it)
-		printf("%ldms :\t[ %d ]%s\n", get_time_since_start(), p->id + 1, msg);
+		printf("%ld %d %s\n", get_time_since_start(), p->id + 1, msg);
 	pthread_mutex_unlock(&(p->tblptr->enough_lock));
 	pthread_mutex_unlock(&(p->tblptr->death_lock));
 	pthread_mutex_unlock(&(p->tblptr->print_lock));
 }
-
 void	*print_too_much_prompt(t_philo_list *p)
 {
 	philo_status_print(p, " Some were trying desperatly to breath\n");
@@ -31,19 +30,17 @@ void	*print_too_much_prompt(t_philo_list *p)
 \"I must eat before they do, or I might die tonight\".\n");
 	philo_status_print(p, " Well not too much of a surprise right ?\n");
 	philo_status_print(p, " You have to realise what you just did i guess\n");
-	philo_status_print(p, " In the meantime could you check how much \
-philosophers you wanted around that dinner table please ?\n\n\n");
 	pthread_mutex_lock(&(p->tblptr->print_lock));
 	pthread_mutex_lock(&(p->tblptr->death_lock));
 	if (!p->tblptr->stop_it)
-		printf("%ldms :\t[ %d ]  %d ?\n\n\n",
+		printf("%ldms :\t[ %d ]Like really ?  %d ?!\n\n\n",
 			get_time_since_start(), p->id + 1, p->tblptr->o.philo_nb);
 	pthread_mutex_unlock(&(p->tblptr->death_lock));
 	pthread_mutex_unlock(&(p->tblptr->print_lock));
 	philo_status_print(p,
-		"I feel like something has got out of hand so ill just leave\n");
+		"I fear your next command, honestly.\n");
 	philo_status_print(p,
-		"Next time try with less or equal to 5000 philosophers.\n");
+		"Next time try with less or equal than \e[33m5000 philosophers.\e[0m\n");
 	smart_sleep(p, p->o.die);
 	philo_status_print(p, "Oh and they are all dead you should know this\n");
 	philo_status_print(p, "You are very welcome, again.\n");
